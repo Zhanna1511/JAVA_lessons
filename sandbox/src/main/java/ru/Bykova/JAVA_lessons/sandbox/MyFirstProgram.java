76f00b870b8926648b08d65a57e414099509bd88
Zhanna Bykova <jannochka.89@mail.ru>
2020-04-26 18:19:42 +0300

    Тренировки с урока

--- sandbox/src/main/java/ru/Bykova/JAVA_lessons/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/Bykova/JAVA_lessons/sandbox/MyFirstProgram.java
@@ -4,7 +4,10 @@ public class MyFirstProgram {
 
    public static void main(String[] args) {
 
-     System.out.println("Hello, world!");
-   }
+       Square s = new Square (5);
+       System.out.println("Площадь кв со стороной " + s.l + "=" + s.area());
 
+       Rectangle r = new Rectangle (4, 6);
+       System.out.println("Площадь прямоуг со сторонами " + r.a + r.b + "=" + r.area());
+   }
 }
