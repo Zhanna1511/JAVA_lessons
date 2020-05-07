97418eef769635c2b1e7796f41d7be8ea954faae
Zhanna Bykova <jannochka.89@mail.ru>
2020-05-07 22:28:21 +0300

    лекц 3.3 запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/Bykova/JAVA_lessons/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/Bykova/JAVA_lessons/addressbook/appmanager/ApplicationManager.java
@@ -1,23 +1,36 @@
 package ru.Bykova.JAVA_lessons.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-
     public WebDriver wd;
 
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private ContactHelper contactHelper;
+    private String browser;
+
+    public ApplicationManager (String browser) {
+
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new ChromeDriver();
+        if (browser == BrowserType.CHROME) {
+            wd = new ChromeDriver();
+        } else if (browser == BrowserType.FIREFOX) {
+            wd = new FirefoxDriver();
+        } else if (browser == BrowserType.IE) {
+            wd = new InternetExplorerDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         contactHelper = new ContactHelper(wd);
@@ -27,13 +40,9 @@ public class ApplicationManager {
         sessionHelper.login("admin", "secret");
     }
 
-    public void fillGroupForm() {
-    }
-
     public void stop() {
         wd.quit();
     }
-
 
     public GroupHelper getGroupHelper() {
 
--- addressbook-web-tests/src/test/java/ru/Bykova/JAVA_lessons/addressbook/model/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/Bykova/JAVA_lessons/addressbook/model/TestBase.java
@@ -1,12 +1,15 @@
 package ru.Bykova.JAVA_lessons.addressbook.model;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.Bykova.JAVA_lessons.addressbook.appmanager.ApplicationManager;
 
+import static org.openqa.selenium.remote.BrowserType.CHROME;
+
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(CHROME);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
