8af2223d7374632aefc974a3c4c0244d9b1ee183
Zhanna Bykova <jannochka.89@mail.ru>
2020-05-13 22:32:29 +0300

    Лекция 4.3 выполнена

--- addressbook-web-tests/src/test/java/ru/Bykova/JAVA_lessons/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/Bykova/JAVA_lessons/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,7 @@
 package ru.Bykova.JAVA_lessons.addressbook.tests;
 
-import org.testng.annotations.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import ru.Bykova.JAVA_lessons.addressbook.model.GroupData;
 import ru.Bykova.JAVA_lessons.addressbook.model.TestBase;
 
@@ -9,7 +10,11 @@ public class GroupCreationTests extends TestBase {
     @Test
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();//выч кол-во групп ДО добавления
         app.getGroupHelper().createGroup(new GroupData("test1",null,null));
+        int after = app.getGroupHelper().getGroupCount();//выч кол-во групп ПОСЛЕ добавления
+        Assert.assertEquals(after, before +1);//кол-во групп,доб.ДО должно совп.с кол-вом групп,доб.ПОСЛЕ,увел-х на ед.
+
     }
 
 }
--- addressbook-web-tests/src/test/java/ru/Bykova/JAVA_lessons/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/Bykova/JAVA_lessons/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.Bykova.JAVA_lessons.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.Bykova.JAVA_lessons.addressbook.model.GroupData;
 import ru.Bykova.JAVA_lessons.addressbook.model.TestBase;
@@ -12,12 +13,13 @@ public class GroupModificationTests extends TestBase {
         if (! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1",null,null));
         }
+        int before = app.getGroupHelper().getGroupCount();
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test1","test1","test1"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
-
-
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before);
     }
 }
