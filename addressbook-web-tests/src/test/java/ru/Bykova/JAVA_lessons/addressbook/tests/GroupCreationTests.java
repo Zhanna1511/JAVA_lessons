729b803244aa41907e02bb409b0559f38f321fd5
Zhanna Bykova <jannochka.89@mail.ru>
2020-05-15 17:23:06 +0300

    Лекция 4.9. Лямбда-выражения выполнена

--- addressbook-web-tests/src/test/java/ru/Bykova/JAVA_lessons/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/Bykova/JAVA_lessons/addressbook/tests/GroupCreationTests.java
@@ -4,7 +4,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.Bykova.JAVA_lessons.addressbook.model.GroupData;
 import ru.Bykova.JAVA_lessons.addressbook.model.TestBase;
-
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,18 +13,12 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        GroupData group = new GroupData("test1",null,null);
+        GroupData group = new GroupData("test2",null,null);
         app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() +1);//размер списка после модификации равен размеру списка До+1
 
-        int max =0;
-        for (GroupData q: after) {
-            if (q.getId() > max) {
-                max = q.getId();
-            }
-        }
-        group.setId(max);
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
