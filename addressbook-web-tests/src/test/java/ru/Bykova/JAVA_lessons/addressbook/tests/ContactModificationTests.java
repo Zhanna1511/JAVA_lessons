bf2cc0690550440caf780f6d9f6903b6331e6b4e
Zhanna Bykova <jannochka.89@mail.ru>
2020-05-20 21:38:14 +0300

    ДЗ 9 тест на Модификацию контакта пока не доделан

--- addressbook-web-tests/src/test/java/ru/Bykova/JAVA_lessons/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/Bykova/JAVA_lessons/addressbook/tests/ContactModificationTests.java
@@ -11,14 +11,14 @@ import java.util.List;
 
 public class ContactModificationTests extends TestBase {
 
-    @Test (enabled = false)
+    @Test
     public void testContactModification() {
-        if (! app.getContactHelper().isThereAContact()) {
-            app.getContactHelper().createContact(new ContactData("Gheorghe", "Alan", "Smith", "Nicky", "Tester", "Kontur", "Leninskiy avenu,168", "7-09-46", "8-924-345-23-34", "345-45-35", "234-45-23", "Email1@mail.ru", "Email2@bk.ru", "Email3@gmail.ru", "vk.com", "12", "March", "1995", "11", "June", "2001","test1", "SPb, Nevsky avenu", "345-56-34", "Fish seller"), true);
+        if (!app.getContactHelper().isThereAContact()) {
+            app.getContactHelper().createContact(new ContactData("Gheorghe", "Alan", "Smith", "Nicky", "Tester", "Kontur", "Leninskiy avenu,168", "7-09-46", "8-924-345-23-34", "345-45-35", "234-45-23", "Email1@mail.ru", "Email2@bk.ru", "Email3@gmail.ru", "vk.com", "12", "March", "1995", "11", "June", "2001", "test1", "SPb, Nevsky avenu", "345-56-34", "Fish seller"), true);
         }
         List<ContactData> before = app.getContactHelper().getContactList();
         app.getContactHelper().selectContact(before.size() - 1);
-        app.getContactHelper().initContactModification();
+        app.getContactHelper().initContactModification(before.size() - 1);
         ContactData contact = new ContactData(before.get(before.size() - 1).getId(),"Gheorghe6", "Alan", "Smith5", "Nicky", "Tester", "Kontur", "Leninskiy avenu,168", "7-09-46", "8-924-345-23-34", "345-45-35", "234-45-23", "Email1@mail.ru", "Email2@bk.ru", "Email3@gmail.ru", "vk.com", "12", "March", "1995", "11", "June", "2001", null, "SPb, Nevsky avenu", "345-56-34", "Fish seller");
         app.getContactHelper().fillContactForms(contact, false);
         //app.getContactHelper().chooseAvatar("\\img\\i380664.jpg");
