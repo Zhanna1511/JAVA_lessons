78425324fc1d48d35b9501d816c77892637a467f
Zhanna Bykova <jannochka.89@mail.ru>
2020-05-25 19:08:28 +0300

    Лек 6.3 опции ком.строки
    Анализ опций командой строки при запуске генератора данных.
    Подкл библ jcommander

--- addressbook-web-tests/src/test/java/ru/Bykova/JAVA_lessons/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/Bykova/JAVA_lessons/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.Bykova.JAVA_lessons.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.Bykova.JAVA_lessons.addressbook.model.GroupData;
 
 import java.io.File;
@@ -11,15 +14,30 @@ import java.util.List;
 
 public class GroupDataGenerator {
 
-    public static void main(String[] args) throws IOException {//массив пар-ов
-        int count = Integer.parseInt(args[0]);//передача данных из ком.строки:1.кол-во групп.
-        File file = new File(args[1]);//путь к файлу
+    @Parameter(names ="-c", description = "Group count")
+    public int count;
 
-        List<GroupData> groups = generateGroups(count);//генерация данных
-        save(groups, file);//сохр-е данных в файл
+    @Parameter(names ="-f", description = "Target file")
+    public String file;
+
+    public static void main(String[] args) throws IOException {//массив пар-ов
+        GroupDataGenerator generator = new GroupDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+        generator.run();
     }
 
-    private static void save(List<GroupData> groups, File file) throws IOException {
+    private void run() throws IOException {
+        List<GroupData> groups = generateGroups(count);//генерация данных
+        save(groups, new File(file));//сохр-е данных в файл
+    }
+
+    private void save(List<GroupData> groups, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file); //откр файл для записи
         for (GroupData group : groups) {//проходимся в цикле по всем группам,кот.нах-ся в списке групс
@@ -28,7 +46,7 @@ public class GroupDataGenerator {
         writer.close();//в конце закрываем файл
     }
 
-    private static List<GroupData> generateGroups(int count) {
+    private List<GroupData> generateGroups(int count) {
         List<GroupData> groups = new ArrayList<GroupData>();
         for (int i = 0; i< count; i++){
             groups.add(new GroupData().withName(String.format("test %s", i))
