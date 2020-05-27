217d9bbcb690e831c071a07b6328873cd15c23be
Zhanna Bykova <jannochka.89@mail.ru>
2020-05-27 20:34:15 +0300

    Лекция 6.7. Формат JSON выполн
    подключили библ json,научились генерировать файл json и загружать тест с таким форматом данных.

--- addressbook-web-tests/src/test/java/ru/Bykova/JAVA_lessons/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/Bykova/JAVA_lessons/addressbook/model/GroupData.java
@@ -1,5 +1,6 @@
 package ru.Bykova.JAVA_lessons.addressbook.model;
 
+import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
@@ -10,8 +11,11 @@ import java.util.Objects;
 public class GroupData {
     @XStreamOmitField //пропустить поле <id>2147483647</id>
     private int id = Integer.MAX_VALUE;
+    @Expose
     private String name;
+    @Expose
     private String header;
+    @Expose
     private String footer;
 
 
