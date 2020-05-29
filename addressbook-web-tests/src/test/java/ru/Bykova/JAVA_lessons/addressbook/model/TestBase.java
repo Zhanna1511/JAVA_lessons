ce9aefebe3750ef985942bb494776f7ea54c73aa
Zhanna Bykova <jannochka.89@mail.ru>
2020-05-30 00:22:20 +0300

    Лекция 6.11. Протоколирование (лог-файлы) выполнена
    Создан файл logback.xml

--- addressbook-web-tests/src/test/java/ru/Bykova/JAVA_lessons/addressbook/model/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/Bykova/JAVA_lessons/addressbook/model/TestBase.java
@@ -1,11 +1,20 @@
 package ru.Bykova.JAVA_lessons.addressbook.model;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.Bykova.JAVA_lessons.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);//переменная для логгера
 
     protected static final ApplicationManager app =
             new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));//взять знач-е систем св-ва "браузер"
@@ -18,5 +27,14 @@ public class TestBase {
     @AfterSuite(alwaysRun = true)
     public void tearDown() throws Exception {
         app.stop();
+    }
+
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));//вывод логов
+    }
+    @AfterMethod (alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info("Stop test " + m.getName());//вывод логов
     }
 }
