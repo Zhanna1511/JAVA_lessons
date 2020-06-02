039ef3c41cd90b21dbb8ec18f2bffa18c29e9ff7
Zhanna Bykova <jannochka.89@mail.ru>
2020-06-02 10:45:35 +0300

    Объединение тестов адреса,телефоны,емейлы контакта

--- addressbook-web-tests/src/test/java/ru/Bykova/JAVA_lessons/addressbook/tests/ContactContactHomePageInfoTests.java
+++ addressbook-web-tests/src/test/java/ru/Bykova/JAVA_lessons/addressbook/tests/ContactContactHomePageInfoTests.java
@@ -8,10 +8,10 @@ import ru.Bykova.JAVA_lessons.addressbook.model.TestBase;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 
-public class ContactEmailTests extends TestBase {
+class ContactHomePageInfoTests extends TestBase {//public class?
 
     @BeforeMethod
     public void  ensurePreconditions() {
@@ -22,19 +22,36 @@ public class ContactEmailTests extends TestBase {
                     "345-45-35").withFax("234-45-23").withEmail("Email1@mail.ru").withGroup("test1"), true);
         }
     }
-
     @Test
     public void  testContactPhones() {
         ContactData contact = app.contact().all().iterator().next();
         ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
+        assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
         assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
+        assertThat(contact.getCompanyAddress(), equalTo(contactInfoFromEditForm.getCompanyAddress()));
     }
 
+    private String mergePhones(ContactData contact) {
+        return Arrays.asList(contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone(), contact.getHomePhone2())
+                .stream().filter((s) -> ! s.equals(""))
+                .map(ContactHomePageInfoTests::cleanedFormat)
+                .collect(Collectors.joining("\n"));
+    }
     private String mergeEmails(ContactData contact) {
         return Arrays.asList(contact.getEmail(), contact.getEmail2(), contact.getEmail3())
                 .stream().filter((s) -> ! s.equals(""))
+                .map(ContactHomePageInfoTests::cleaned)
                 .collect(Collectors.joining("\n"));
+    }
+
+    public static String cleanedFormat(String phone) {
+        return phone.replaceAll("\\s","")
+                .replaceAll("[-()]","");
+    }
+
+    public static String cleaned(String address) {
+        return address.replaceAll("\\s+"," ").trim();
     }
 
 }
