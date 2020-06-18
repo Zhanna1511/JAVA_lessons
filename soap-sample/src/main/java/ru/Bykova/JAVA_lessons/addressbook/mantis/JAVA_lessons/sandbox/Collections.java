cef4c25d9d0d3e960c722911487392b32e4f8836
Zhanna Bykova <jannochka.89@mail.ru>
2020-06-18 19:39:41 +0300

    лек 9.2 начало

---
+++ soap-sample/src/main/java/ru/Bykova/JAVA_lessons/addressbook/mantis/JAVA_lessons/sandbox/Collections.java
@@ -0,0 +1,32 @@
+package ru.Bykova.JAVA_lessons.addressbook.mantis.JAVA_lessons.sandbox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collections {
+
+    public static void main(String[] args) {
+        String[] langs = {"java","C#","Python","PHP"};
+        /*или можно в Неск строк:
+        String[] langs = new String[4];//объявлена переменная типа массив строк-в этом массиве будем хранить названия языков прогр.
+        langs[0] = "java";//заполн.данными
+        langs[1] ="C#";
+        langs[2] ="Python";
+        langs[3] ="PHP";
+         */
+        List<String> languages = new ArrayList<String>();
+        languages.add("Java");
+        languages.add("C#");
+        languages.add("Python");
+        //или 1стр: List<String> languages = new Arrays.asList("java","C#","Python","PHP");
+
+        for (int i = 0; i < languages.size();i++) {
+            System.out.println("Я хочу выучить " + languages.get(i));
+        }
+        /*либо:
+        for (int i=0; i < langs.length; i++) {//прох.по кажд.эл
+            System.out.println("Я хочу выучить " + langs[i]);
+            }
+             */
+    }
+}
