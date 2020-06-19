29425da6c652506bc41dce222145bbc4ae5f2c80
Zhanna Bykova <jannochka.89@mail.ru>
2020-06-19 15:17:27 +0300

    лек 9.5 не додел

---
+++ mantis-tests/src/test/java/ru/Bykova/JAVA_lessons/mantis/tests/SoapTests.java
@@ -0,0 +1,34 @@
+package ru.Bykova.JAVA_lessons.mantis.tests;
+
+import org.testng.annotations.Test;
+import ru.Bykova.JAVA_lessons.mantis.model.Issue;
+import ru.Bykova.JAVA_lessons.mantis.model.Project;
+
+import javax.xml.rpc.ServiceException;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
+import java.util.Set;
+
+import static org.testng.Assert.*;
+
+public class SoapTests extends TestBase {
+
+    @Test
+    public void testGetProject() throws MalformedURLException, ServiceException, RemoteException {//получ список проектов
+        Set<Project> projects = app.soap().getProjects();
+        System.out.println(projects.size());
+        for (Project project : projects) {
+            System.out.println(project.getName());
+        }
+    }
+
+    @Test
+    public void testCreateIssue() throws RemoteException, ServiceException, MalformedURLException {//баг-репорт
+        skipIfNotFixed(2);
+        Set<Project> projects = app.soap().getProjects();
+        Issue issue = new Issue().withSummary("Test issue").withDescription("Test issue description")
+                .withProject(projects.iterator().next());
+        Issue created = app.soap().addIssue(issue);//новый объект сравниваем с существующим
+        assertEquals(issue.getSummary(), created.getSummary());
+    }
+}
