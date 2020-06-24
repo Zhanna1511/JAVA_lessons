ec126211e965dfed23b61b11ea05bdcc5f8fa76f
Zhanna Bykova <jannochka.89@mail.ru>
2020-06-24 12:49:52 +0300

    задание 20

--- rest-sample/src/test/java/ru/Bykova/JAVA_lessons/rest/Issue.java
+++ rest-sample/src/test/java/ru/Bykova/JAVA_lessons/rest/Issue.java
@@ -7,26 +7,40 @@ public class Issue {
     private int id;
     private String subject;
     private String description;
+    private  String state_name;
 
     public int getId() {
         return id;
     }
-
     public Issue withId(int id) {
         this.id = id;
         return this;
     }
-
     public String getSubject() {
         return subject;
     }
-
     public Issue withSubject(String subject) {
         this.subject = subject;
         return this;
     }
 
+    public String getState_name() {
+        return state_name;
+    }
 
+    public Issue withState_name(String state_name) {
+        this.state_name = state_name;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Issue withDescription(String description) {
+        this.description = description;
+        return this;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -43,12 +57,4 @@ public class Issue {
         return Objects.hash(id, subject, description);
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public Issue withDescription(String description) {
-        this.description = description;
-        return this;
-    }
 }
--- rest-sample/src/test/java/ru/Bykova/JAVA_lessons/rest/RestTests.java
+++ rest-sample/src/test/java/ru/Bykova/JAVA_lessons/rest/RestTests.java
@@ -6,27 +6,30 @@ import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
-import org.testng.annotations.Test;
 import org.apache.http.message.BasicNameValuePair;
+import org.testng.SkipException;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.util.Set;
+
 import static org.testng.Assert.assertEquals;
 
-public class RestTests {
+public class RestTests  {
 
     @Test
     public void testCreateIssue() throws IOException {
-        Set<Issue> oldIssues = getIssues();//получ мн-во объектов типа issue
-        Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");;//созд нов объект
+        skipIfNotFixed(0000001);
+        Set<Issue> oldIssues = getIssues();
+        Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");
         int issueID = createIssue(newIssue);
-        Set<Issue> newIssues = getIssues();//получ нов набор
-        oldIssues.add(newIssue.withId(issueID));//доб в старое мн-во объект
+        Set<Issue> newIssues = getIssues();
+        oldIssues.add(newIssue.withId(issueID));
         assertEquals(newIssues, oldIssues);
     }
 
     private Set<Issue> getIssues() throws IOException {
-        String json = getExecutor().execute(Request.Get("https://bugify.stqa.ru/api/issues.json?limit=500"))//хотим получить список всех баг-репортов в формате json
+        String json = getExecutor().execute(Request.Get("https://bugify.stqa.ru/api/issues.json?limit=100"))
                 .returnContent().asString();
         JsonElement parsed = new JsonParser().parse(json);
         JsonElement issues = parsed.getAsJsonObject().get("issues");
@@ -38,11 +41,32 @@ public class RestTests {
     }
 
     private int createIssue(Issue newIssue) throws IOException {
-        String json = getExecutor().execute(Request.Post("https://bugify.stqa.ru/api/issues.json")
+        String json = getExecutor().execute(Request.Post("https://bugify.stqa.ru/api/issues.json?limit=100")
                 .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
                         new BasicNameValuePair("description", newIssue.getDescription())))
                 .returnContent().asString();
         JsonElement parsed = new JsonParser().parse(json);
         return parsed.getAsJsonObject().get("issue_id").getAsInt();
+    }
+
+    public void skipIfNotFixed(int issueId) throws IOException {
+        if (isIssueOpen(issueId)) {
+            throw new SkipException("Ignored because of issue " + issueId);
+        }
+    }
+
+    private boolean isIssueOpen(int issueId) throws IOException {
+        Issue issue = getIssue(issueId);
+        return  (issue.getState_name().equals("Open") || issue.getState_name().equals("In Progress")
+                || issue.getState_name().equals("Re-opened"));
+    }
+
+    private Issue getIssue(int issueId) throws IOException {
+        String json = getExecutor().execute(Request.Get("https://bugify.stqa.ru/api/issues/" + issueId + ".json"))
+                .returnContent().asString();
+        JsonElement parsed = new JsonParser().parse(json);
+        JsonElement issues = parsed.getAsJsonObject().get("issues");
+        Set<Issue> issueWithId = new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
+        return issueWithId.iterator().next();
     }
 }
